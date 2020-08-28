411816a9f82065f3598867da9fb5bf524997a268
PotatoPresident <potatoboy9999@outlook.com>
2020-08-28 16:01:39 -0700

    Added Offline Player Support

--- src/main/java/us/potatoboy/invview/ViewCommand.java
+++ src/main/java/us/potatoboy/invview/ViewCommand.java
@@ -1,47 +1,68 @@
 package us.potatoboy.invview;
 
-import com.google.gson.internal.$Gson$Preconditions;
-import com.mojang.brigadier.Command;
-import com.mojang.brigadier.CommandDispatcher;
-import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.authlib.GameProfile;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
-import net.minecraft.block.ChestBlock;
-import net.minecraft.block.entity.ChestBlockEntity;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.command.argument.EntityArgumentType;
-import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.command.argument.GameProfileArgumentType;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.EnderChestInventory;
-import net.minecraft.screen.*;
-import net.minecraft.server.command.CommandManager;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.NbtIo;
+import net.minecraft.screen.GenericContainerScreenHandler;
+import net.minecraft.screen.ScreenHandlerType;
+import net.minecraft.screen.SimpleNamedScreenHandlerFactory;
+import net.minecraft.screen.slot.Slot;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.world.GameMode;
+import net.minecraft.util.Util;
+import net.minecraft.util.WorldSavePath;
+import org.apache.logging.log4j.LogManager;
 
-import java.util.Collection;
+import java.io.File;
 
 public class ViewCommand {
-    public static int inv(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
-        ServerPlayerEntity player = context.getSource().getPlayer();
-        ServerPlayerEntity requestedPlayer = EntityArgumentType.getPlayer(context, "target");
-        PlayerInventory requestedInventory = requestedPlayer.inventory;
-        CombinedInventory combinedInventory = new CombinedInventory(requestedInventory.main, requestedInventory.armor, requestedInventory.offHand);
+    private static MinecraftServer minecraftServer = InvView.getMinecraftServer();
 
-        player.openHandledScreen(new SimpleNamedScreenHandlerFactory((syncId, inv, playerEntity) -> {
-            return new GenericContainerScreenHandler(ScreenHandlerType.GENERIC_9X5, syncId, player.inventory, combinedInventory, 5);
-        }, requestedPlayer.getDisplayName()));
+    public static int Inv(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
+        ServerPlayerEntity player = context.getSource().getPlayer();
+        ServerPlayerEntity requestedPlayer = GetRequestedPlayer(context);
+        PlayerInventory requestedInventory = requestedPlayer.inventory;
+
+        CombinedInv combinedInventory = new CombinedInv(requestedPlayer);
+        player.openHandledScreen(new SimpleNamedScreenHandlerFactory((syncId, inv, playerEntity) ->
+                new GenericContainerScreenHandler(ScreenHandlerType.GENERIC_9X5, syncId, player.inventory, combinedInventory, 5),
+                requestedPlayer.getDisplayName()
+        ));
+
         return 1;
     }
 
-    public static int echest(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
+    public static int EChest(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
         ServerPlayerEntity player = context.getSource().getPlayer();
-        ServerPlayerEntity requestedPlayer = EntityArgumentType.getPlayer(context, "target");
-        EnderChestInventory requestedEchest = requestedPlayer.getEnderChestInventory();
+        ServerPlayerEntity requestedPlayer = GetRequestedPlayer(context);
+        EnderChestSavable requestedEchest = new EnderChestSavable(requestedPlayer);
 
-        player.openHandledScreen(new SimpleNamedScreenHandlerFactory((syncId, inv, playerEntity) -> {
-            return GenericContainerScreenHandler.createGeneric9x3(syncId, player.inventory, requestedEchest);
-        }, requestedPlayer.getDisplayName()));
+        player.openHandledScreen(new SimpleNamedScreenHandlerFactory((syncId, inv, playerEntity) ->
+                GenericContainerScreenHandler.createGeneric9x3(syncId, player.inventory, requestedEchest),
+                requestedPlayer.getDisplayName()
+        ));
+
         return 1;
+    }
+
+    private static ServerPlayerEntity GetRequestedPlayer(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
+        GameProfile requestedProfile = GameProfileArgumentType.getProfileArgument(context, "target").iterator().next();
+        ServerPlayerEntity requestedPlayer = minecraftServer.getPlayerManager().getPlayer(requestedProfile.getName());
+
+        if (requestedPlayer == null) {
+            requestedPlayer = minecraftServer.getPlayerManager().createPlayer(requestedProfile);
+            minecraftServer.getPlayerManager().loadPlayerData(requestedPlayer);
+        }
+
+        return requestedPlayer;
     }
 }
