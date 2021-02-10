42e5441d942a14168edcaa171f1daf74adc990b1
PotatoPresident <potatoboy9999@outlook.com>
2021-02-10 08:39:50 -0800

    Renamed permissions Fixed offline players not working with luck perms

--- src/main/java/us/potatoboy/invview/gui/MountScreenHandler.java
+++ src/main/java/us/potatoboy/invview/gui/MountScreenHandler.java
@@ -1,14 +1,13 @@
 package us.potatoboy.invview.gui;
 
-import net.minecraft.entity.passive.HorseBaseEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.screen.HorseScreenHandler;
 
 public class MountScreenHandler extends HorseScreenHandler {
-    public MountScreenHandler(int syncId, PlayerInventory playerInventory, Inventory inventory, HorseBaseEntity entity) {
-        super(syncId, playerInventory, inventory, entity);
+    public MountScreenHandler(int syncId, PlayerInventory playerInventory, Inventory inventory) {
+        super(syncId, playerInventory, inventory, null);
     }
 
     @Override
